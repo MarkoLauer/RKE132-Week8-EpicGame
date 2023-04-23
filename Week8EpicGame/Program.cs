be6db7588713725f9aa2de8cea5b702f818de71e
MarkoLauer <marko.lauer@tktk.ee>
2023-04-23 21:38:50 +0300

    add week8 Epic Game

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+string folderPath = @"C:\Users\Marko";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+//string[] heroes = {"Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+//string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron" };
+string[] weapon = {"Magic Wand", "plastic fork", "wooden sword", "banana", "Lego brick"};
+
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} saves the day with {heroWeapon}");
+
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} tries to take over the world with {villainWeapon}");
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStrinFromArray = someArray[randomIndex];
+    return randomStrinFromArray;
+}
